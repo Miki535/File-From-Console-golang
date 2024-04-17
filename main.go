5b94f9a44ae930ce8a10c1239e948db78169af75
Taras Shcherban <147646967+Miki535@users.noreply.github.com>
2024-04-17 13:15:18 +0000

    Update main.go
    Add coments and new info for user

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"log"
 	"os"
 	"os/user"
 	"path/filepath"
@@ -15,21 +16,28 @@ func main() {
 	var startinput string
 	fmt.Println("Hello USERS!")
 	for {
-		fmt.Println("Enter start to start((")
+		fmt.Println("Enter start to start((\n And any word to exit")
+
 		fmt.Scan(&startinput)
 		startinput2 := strings.ToLower(startinput)
 		if startinput2 == "start" {
+			// add file code:
 			fmt.Println("Enter name for file:")
 			fmt.Scan(&Nameforfile)
 			fmt.Println("Name add succesfuly")
 			fmt.Println("Enter infomation into youre file:")
+			// special scan for info in our file
 			reader := bufio.NewReader(os.Stdin)
 			Infoinfile, _ = reader.ReadString('\n')
 			Infoinfile = strings.TrimSpace(Infoinfile)
 			fmt.Println("Information add succesfuly")
+			// informing the user about the date the file was added.
+			log.Println("Data of added file,", Nameforfile)
+			// add file into downloads directory
 			FILE(Nameforfile, Infoinfile)
 			fmt.Println("Thanks for using our program!!!")
 		} else {
+			// exit program
 			os.Exit(0)
 		}
 	}
@@ -41,21 +49,21 @@ func FILE(FILEName string, INFO string) {
 		fmt.Println(err)
 		return
 	}
-
+	// join into Downloads directory
 	downloadsPath := filepath.Join(userInfo.HomeDir, "Downloads")
 
 	filepath := filepath.Join(downloadsPath, FILEName)
 
 	file, err := os.Create(filepath)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error creating file!", err)
 		return
 	}
 	defer file.Close()
 
 	_, err = file.WriteString(INFO)
 	if err != nil {
-		fmt.Println(err)
+		fmt.Println("Error in writing info in file!", err)
 		return
 	}
 }
